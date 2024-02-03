dacd60265a85cfecaf4d2c4eefc0bc8937c57c3d
brani <branislannjemec@gmail.com>
2024-02-03 11:26:54 +0100

    add more validators

--- lib/validator.ts
+++ lib/validator.ts
@@ -2,5 +2,19 @@ import * as z from "zod";
 
 export const surveyFormSchema = z.object({
   email: z.string(),
-  phoneNumber: z.number(),
+  telefon: z.number(),
+  visina: z.string(),
+  tezina: z.string(),
+  datumRodjenja: z.date(),
+  radniStatus: z.string(),
+  radnoVreme: z.string().optional(),
+  brojObroka: z.string(),
+  satiSpavanja: z.string(),
+  tipOsobe: z.string(),
+  zdravstveniProblem: z.string(),
+  dodatno: z.string(),
+  razlogPrestanka: z.string(),
+  imaliTrenera: z.string(),
+  ocekivanja: z.string(),
+  ranijeTrenirali: z.string(),
 });
