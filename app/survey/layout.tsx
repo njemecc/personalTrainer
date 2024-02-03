904ccab7abe88ffe90e834ac66eafbd7be23a31d
brani <branislannjemec@gmail.com>
2024-02-03 11:25:22 +0100

    survey layout styles

--- app/survey/layout.tsx
+++ app/survey/layout.tsx
@@ -1,10 +1,12 @@
+import styles from "./surveyLayout.module.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen flex-col ">
+    <div className={styles["survey-layout-wrapper"]}>
       <main className="flex-1">{children}</main>
     </div>
   );
