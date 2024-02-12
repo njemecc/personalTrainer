679437faad987ba81d3ae021ead853eec22738ba
brani <branislannjemec@gmail.com>
2024-02-12 18:22:15 +0100

    put About and Testimonials in the same section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,30 @@
+"use client";
 import { Hero2 } from "@/components/features/home/Hero2";
 import { Pricing } from "@/components/features/home/Pricing";
 
 import About from "@/components/features/home/About";
 import Testimonials from "@/components/features/home/Testimonials";
 
+import ReactPlayer from "react-player/lazy";
+
 export default function HomePage() {
   return (
     <main>
-      <section className="h-screen">
+      <section className="h-1/3">
         <Hero2 />
       </section>
-      <section>
-        <section>
-          <Testimonials />
-        </section>
+      <section
+        style={{
+          background:
+            "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)),url(/assets/images/tiangle.jpg)",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+
+          clipPath: "polygon(100% 0, 100% 91%, 49% 100%, 0 89%, 0 0)",
+        }}
+      >
         <About />
+        <Testimonials />
       </section>
       <section>
         <Pricing />
