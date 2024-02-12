69934e7df2e5f45956946f946b82bcef46c85529
brani <branislannjemec@gmail.com>
2024-02-12 22:09:49 +0100

    add different imports

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,19 @@
-"use client";
-import { Hero2 } from "@/components/features/home/Hero2";
-import { Pricing } from "@/components/features/home/Pricing";
+//styles
+import styles from "./RootPage.module.css";
 
-import About from "@/components/features/home/About";
-import Testimonials from "@/components/features/home/Testimonials";
+import { Hero } from "@/components/features/home/hero/Hero";
+import { Pricing } from "@/components/features/home/pricing/Pricing";
 
-import ReactPlayer from "react-player/lazy";
+import About from "@/components/features/home/about/About";
+import Testimonials from "@/components/features/home/testimonials/Testimonials";
 
 export default function HomePage() {
   return (
     <main>
       <section className="h-1/3">
-        <Hero2 />
+        <Hero />
       </section>
-      <section
-        style={{
-          background:
-            "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)),url(/assets/images/tiangle.jpg)",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-
-          clipPath: "polygon(100% 0, 100% 91%, 49% 100%, 0 89%, 0 0)",
-        }}
-      >
+      <section className={styles["about-testimonials-wrapper"]}>
         <About />
         <Testimonials />
       </section>
