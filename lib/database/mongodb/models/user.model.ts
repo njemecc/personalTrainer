2bc9de2da7e1b177ee9c1e5ba2bb7e49d1d3988c
brani <branislannjemec@gmail.com>
2024-03-02 18:00:35 +0100

    delete firstName, lastName and email that i am expecting

--- lib/database/mongodb/models/user.model.ts
+++ lib/database/mongodb/models/user.model.ts
@@ -3,9 +3,6 @@ import { model, Schema, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String },
-  lastName: { type: String },
   photo: { type: String },
 });
 
