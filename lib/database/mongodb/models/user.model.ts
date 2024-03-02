cd8a0d5cfeb35231aceab20cd9ee34ac7fc81067
brani <branislannjemec@gmail.com>
2024-03-02 19:34:19 +0100

    add username

--- lib/database/mongodb/models/user.model.ts
+++ lib/database/mongodb/models/user.model.ts
@@ -4,6 +4,7 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   photo: { type: String },
+  username: { type: String },
 });
 
 const User = models.User || model("User", UserSchema);
