c3d048fa8a1d79ddf1080713138e7046cb65a4c7
brani <branislannjemec@gmail.com>
2024-03-04 23:58:06 +0100

    create admin/users page

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,11 @@
+import { UsersTable } from "@/components/features/admin/users/UsersTable";
+
+const page = () => {
+  return (
+    <div>
+      <UsersTable />
+    </div>
+  );
+};
+
+export default page;
