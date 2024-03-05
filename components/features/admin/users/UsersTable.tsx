9de0b45a7f0a0b5a74a8a33e358b7fd9f13398f9
brani <branislannjemec@gmail.com>
2024-03-05 17:55:28 +0100

    add pagination, sorting and data to the database

--- components/features/admin/users/UsersTable.tsx
+++ components/features/admin/users/UsersTable.tsx
@@ -1,7 +1,212 @@
-import React from "react";
+"use client";
+
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+  SortingState,
+  VisibilityState,
+  getSortedRowModel,
+} from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { columns } from "./UserTableColumns";
+
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { UserColumnParams } from "@/types/users";
 
 const UsersTable = () => {
-  return <div></div>;
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  const data: UserColumnParams[] = [
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.04.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.06.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+    {
+      email: "branislavnjemec@gmail.com",
+      username: "bnjemec01",
+      created_at: new Date("02.03.2023"),
+    },
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnVisibility,
+    },
+  });
+
+  return (
+    <>
+      <div className="flex items-center py-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Kolone
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Nema rezultata.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default UsersTable;
