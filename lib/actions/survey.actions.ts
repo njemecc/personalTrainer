f2e382b7363a3c9440a972e3fea3cce787c18776
brani <branislannjemec@gmail.com>
2024-03-08 15:03:46 +0100

    create getUserAndSurveInfo server action

--- lib/actions/survey.actions.ts
+++ lib/actions/survey.actions.ts
@@ -1,15 +1,34 @@
 "use server";
 
-import { SurveyParams } from "@/types/survey";
+import { SurveyParams, UserSurveyDTO } from "@/types/survey";
 import { connectToDatabase } from "../database/mongodb";
 import Survey from "../database/mongodb/models/survey.model";
 import { handleError } from "../utils";
+import User from "../database/mongodb/models/user.model";
+import { mapUserSurveyToDTO } from "../database/mongodb/mappers/surveyMapper";
 
 export const createSurvey = async (surveyData: SurveyParams) => {
   try {
     await connectToDatabase();
     const newSurvey = await Survey.create(surveyData);
     return JSON.parse(JSON.stringify(newSurvey));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getUserAndSurveyInfo = async (Id: string) => {
+  try {
+    await connectToDatabase();
+    const user = await User.findById(Id).exec();
+
+    const survey = await Survey.findOne({ userId: Id }).exec();
+
+    if (!user || !survey) return null;
+
+    const userInfo: UserSurveyDTO = mapUserSurveyToDTO(user, survey);
+
+    return userInfo;
   } catch (error) {
     handleError(error);
   }
