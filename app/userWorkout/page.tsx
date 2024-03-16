126ff62c25c478282e7e2b6bb7cf2920627acb04
brani <branislannjemec@gmail.com>
2024-03-16 12:37:34 +0100

    create dummy UserWorkout page

---
+++ app/userWorkout/page.tsx
@@ -0,0 +1,11 @@
+import UserWorkout from "@/components/features/user/UserWorkout";
+
+const page = () => {
+  return (
+    <div>
+      <UserWorkout />
+    </div>
+  );
+};
+
+export default page;
