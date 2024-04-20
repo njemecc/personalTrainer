d9ff51ac848a8f86eb5949bf58a274cfef023137
njemecc <branislannjemec@gmail.com>
2024-04-20 16:30:19 +0200

    create createWorkotPlan server action

--- lib/actions/workoutplan.actions.ts
+++ lib/actions/workoutplan.actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { CreateWorkoutPlanParams } from "@/types/workoutPlan";
 import { connectToDatabase } from "../database/mongodb";
 import WorkoutPlan from "../database/mongodb/models/workoutplan.model";
 import { handleError } from "../utils";
@@ -13,6 +14,36 @@ export const getWorkoutplanByUserId = async (id: string) => {
     if (!workoutPlan) return null;
 
     return JSON.parse(JSON.stringify(workoutPlan));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const createWorkoutPlan = async (
+  newWorkout: CreateWorkoutPlanParams
+) => {
+  try {
+    await connectToDatabase();
+
+    const workoutPlanExists = await WorkoutPlan.findOne({
+      userId: newWorkout.userId,
+    });
+
+    let newWorkoutPlan: CreateWorkoutPlanParams;
+
+    if (workoutPlanExists === null) {
+      newWorkoutPlan = await WorkoutPlan.create(newWorkout);
+    } else {
+      newWorkoutPlan = workoutPlanExists;
+      newWorkoutPlan.days.push(newWorkout.days[0]);
+
+      await WorkoutPlan.updateOne(
+        { userId: newWorkout.userId },
+        newWorkoutPlan
+      );
+    }
+
+    return JSON.parse(JSON.stringify(newWorkoutPlan));
   } catch (error) {
     handleError(error);
   }
