e871c016e279532a7d3586ebe593d38378fd0e91
njemecc <branislannjemec@gmail.com>
2024-08-08 08:32:10 +0200

    comment youtube regex

--- lib/validations/createUpdateExerciseForm/createUpdateExerciseFormValidator.ts
+++ lib/validations/createUpdateExerciseForm/createUpdateExerciseFormValidator.ts
@@ -5,11 +5,10 @@ export const createUpdateExerciseFormSchema = z.object({
   exerciseName: z.string({
     required_error: "Naziv vežbe je obavezno polje.",
   }),
-  exerciseUrl: z
-    .string({
-      required_error: "Link ka video snimku je obavezno polje.",
-    })
-    .regex(youtubeEmbedUrlPattern, "Video url mora biti u dobrom formatu!"),
+  exerciseUrl: z.string({
+    required_error: "Link ka video snimku je obavezno polje.",
+  }),
+  // .regex(youtubeEmbedUrlPattern, "Video url mora biti u dobrom formatu!")
   exerciseReps: z.string({
     required_error: "Broj ponavljanja je obavezno polje.",
   }),
