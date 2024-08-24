9adbed0be5a277405ffc10fe2985ae9330bbe69e
njemecc <branislannjemec@gmail.com>
2024-08-24 14:37:47 -0600

    update validation Scheme

--- lib/validations/createUpdateExerciseForm/createUpdateExerciseFormValidator.ts
+++ lib/validations/createUpdateExerciseForm/createUpdateExerciseFormValidator.ts
@@ -2,13 +2,15 @@ import { youtubeEmbedUrlPattern } from "@/constants";
 import * as z from "zod";
 
 export const createUpdateExerciseFormSchema = z.object({
-  exerciseName: z.string({
-    required_error: "Naziv vežbe je obavezno polje.",
+  exercise: z.object({
+    name: z.string({
+      required_error: "Naziv vežbe je obavezno polje.",
+    }),
+    azureName: z.string({
+      required_error: "Naziv vežbe na Azure je obavezno polje.",
+    })
+
   }),
-  exerciseUrl: z.string({
-    required_error: "Link ka video snimku je obavezno polje.",
-  }),
-  // .regex(youtubeEmbedUrlPattern, "Video url mora biti u dobrom formatu!")
   exerciseReps: z.string({
     required_error: "Broj ponavljanja je obavezno polje.",
   }),
