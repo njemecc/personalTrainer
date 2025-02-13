ecbabeacee3870f60474ef0a06743f9f4af67ede
njemecc <branislannjemec@gmail.com>
2025-02-13 23:48:17 +0100

    change socials

--- components/features/home/about/TrainerCard.tsx
+++ components/features/home/about/TrainerCard.tsx
@@ -6,22 +6,22 @@ import { SlSocialInstagram } from "react-icons/sl";
 const TrainerCard = () => {
   return (
     <div className={`${styles.item} text-center duration-1000`}>
-      <img src="/assets/images/stoji-original.jpg" />
+      <img src="/assets/images/djura.jpeg" />
       <div className={styles.inner}>
         <div className={`${styles.info} bg-primary`}>
-          <h4>Marko Gligorijević</h4>
+          <h4>Djura Blažu</h4>
           <p>Personalni Trener</p>
           <div className={styles.links}>
-            <a href="https://www.facebook.com/markomarerema.gligorijevic">
+            <a href="https://www.facebook.com/blazu.djura">
               <img src="/assets/icons/linkedin.svg" />
             </a>
-            <a href="https://www.instagram.com/makson_g7/">
+            <a href="https://www.instagram.com/d_djusi.trener/">
               <img src="/assets/icons/instagram.svg" />
             </a>
-            <a href="#">
+            <a href="https://www.facebook.com/blazu.djura">
               <img src="/assets/icons/whatsapp.svg" />
             </a>
-            <a href="https://api.whatsapp.com/send/?phone=%2B38164692950&text&type=phone_number&app_absent=0">
+            <a href="https://www.facebook.com/blazu.djura">
               <img src="/assets/icons/facebook.svg" />
             </a>
           </div>
