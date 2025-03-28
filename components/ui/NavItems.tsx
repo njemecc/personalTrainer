852e5d2ba938e84aec84c8b02e7cfe2bc78326d5
njemecc <branislannjemec@gmail.com>
2025-03-28 21:13:18 +0100

    add navbar rerendering

--- components/ui/NavItems.tsx
+++ components/ui/NavItems.tsx
@@ -6,43 +6,54 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-//icons
-
 const NavItems = () => {
   const pathName = usePathname();
   const { user } = useUser();
+
+  const isSurveyCompleted = user?.publicMetadata?.isSurveyCompleted;
+
   return (
     <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
-      {headerLinks.map((link) => {
-        const isActive = pathName === link.route;
+      {headerLinks
+        .filter((link) => {
+          if (link.route === "/plan") {
+            return isSurveyCompleted; // Prikazuj "Moj plan" samo ako je survey završen
+          }
 
-        return (
-          <li
-            key={link.route}
-            className={`
-            ${
-              isActive && "text-primary-500"
-            } flex-center p-medium-16 whitespace-nowrap transition-all duration-300  hover:text-gold hover:font-bold`}
-          >
-            {link.route != "/admin" ? (
-              <Link
-                href={
-                  link.route == "/plan"
-                    ? `/plan/${user?.publicMetadata.userId}`
-                    : link.route
-                }
-              >
-                {" "}
-                {link.label}
-              </Link>
-            ) : (
-              <Protect role="org:king">
-                <Link href={link.route}> {link.label}</Link>
-              </Protect>
-            )}
-          </li>
-        );
-      })}
+          if (link.route === "/survey") {
+            return !isSurveyCompleted; // Prikazuj "Anketa" samo ako NIJE završena
+          }
+
+          return true; // Sve ostale rute prikazuj normalno
+        })
+        .map((link) => {
+          const isActive = pathName === link.route;
+
+          return (
+            <li
+              key={link.route}
+              className={`${
+                isActive && "text-primary-500"
+              } flex-center p-medium-16 whitespace-nowrap transition-all duration-300  hover:text-gold hover:font-bold`}
+            >
+              {link.route !== "/admin" ? (
+                <Link
+                  href={
+                    link.route === "/plan"
+                      ? `/plan/${user?.publicMetadata?.userId}`
+                      : link.route
+                  }
+                >
+                  {link.label}
+                </Link>
+              ) : (
+                <Protect role="org:king">
+                  <Link href={link.route}>{link.label}</Link>
+                </Protect>
+              )}
+            </li>
+          );
+        })}
     </ul>
   );
 };
